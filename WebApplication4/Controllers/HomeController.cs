a39b1f3805f2fadc70032fdbf832564a28d7f5e3
altuğ altuğay <aytug327w@gmail.com>
2021-10-07 22:19:48 +0300

    insterted nav-bar but not responsive for now

--- WebApplication4/Controllers/HomeController.cs
+++ WebApplication4/Controllers/HomeController.cs
@@ -8,23 +8,12 @@ namespace WebApplication4.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Anasayfa()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
